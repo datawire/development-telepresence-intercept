dae5cddb809ed9de9572351c28686c2416f0f325
Arturo Gonzalez Villa <arturogonzalez@datawire.io>
2022-05-25 17:11:43 -0500

    add the input to the action

--- connect/connect.js
+++ connect/connect.js
@@ -30,7 +30,7 @@ const telepresenceConnect = async function () {
     }
 
     const files = [`/home/runner/.config/telepresence/id`];
-    const uploadResponse = await artifactClient.uploadArtifact(telepresenceIdArtifact, files, path, artifactOptionsUpload);
+    const uploadResponse = await artifactClient.uploadArtifact(telepresenceIdArtifact, files, '/home/runner/.config/telepresence', artifactOptionsUpload);
     uploadResponse.failedItems.forEach(itemFailed => {
         core.warning(`It was not possible to save the file: ${itemFailed}`);
     });
