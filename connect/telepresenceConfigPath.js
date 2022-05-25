ec9dd43b7b751359feee9a7e00ce5d7bb1ab93fb
Arturo Gonzalez Villa <arturogonzalez@datawire.io>
2022-05-25 17:38:49 -0500

    add the input to the action

--- connect/telepresenceConfigPath.js
+++ connect/telepresenceConfigPath.js
@@ -1,9 +1,9 @@
 exports.getTelepresenceConfigPath = () => {
     switch (process.platform) {
         case "darwin":
-            return `$HOME/Library/Application\\ Support/telepresence`;
+            return `/home/runner/Library/Application\\ Support/telepresence`;
         case "linux":
-            return `$HOME/.config/telepresence`;
+            return `/home/runner/.config/telepresence`;
         default:
             core.setFailed(`The platform ${process.platform} is not supported yet`);
             return None;
