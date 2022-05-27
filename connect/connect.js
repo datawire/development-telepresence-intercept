abf0c2b50fe16680232e3306f2bb2a77a3eb4168
Arturo Gonzalez Villa <arturogonzalez@datawire.io>
2022-05-27 00:26:45 -0500

    compile action

--- connect/connect.js
+++ connect/connect.js
@@ -1,37 +1,20 @@
 const core = require('@actions/core');
-const io = require('@actions/io');
 const exec = require('@actions/exec');
-const artifact = require('@actions/artifact');
-const secrets = require('../src/secrets/index');
+const secrets = require('../src/secrets/dist/index');
 const fs = require('fs');
 const getTelepresenceConfigPath = require('./telepresenceConfigPath');
 
-const artifactClient = artifact.create();
-const artifactOptions = {
-    createArtifactFolder: false,
-};
-const artifactOptionsUpload = {
-    continueOnError: true
-}
-
 const telepresenceConnect = async function () {
-    const telepresenceIdArtifact = core.getInput("telepresence-id-artifact");
     const path = getTelepresenceConfigPath.getTelepresenceConfigPath();
-
-    try {
-        await io.mkdirP(path);
-        await artifactClient.downloadArtifact(telepresenceIdArtifact, path, artifactOptions);
-    } catch (error) {
-        const telepresenceIDSecret = input.getInput('telepresence-id');
-        if (!telepresenceIDSecret || telepresenceIDSecret === '') {
-            core.saveState(secrets.TELEPRESENCE_ID_STATE, secrets.SAVES_TELEPRESENCE_ID);
-            core.warning('Unable to get a previous telepresence id.');
-        } else {
-            try {
-                fs.writeFileSync(`${path}/id`, telepresenceIDSecret);
-            } catch (err) {
-                core.warning(err);
-            }
+    const telepresenceID = core.getInput('telepresence-id');
+    if (!telepresenceID || telepresenceID === '') {
+        core.saveState(secrets.TELEPRESENCE_ID_STATE, secrets.SAVES_TELEPRESENCE_ID);
+        core.warning('Unable to get a previous telepresence id.');
+    } else {
+        try {
+            fs.writeFileSync(`${path}/id`, telepresenceIDSecret);
+        } catch (err) {
+            core.warning(err);
         }
     }
 
@@ -40,12 +23,6 @@ const telepresenceConnect = async function () {
     } catch (error) {
         core.setFailed(error.message);
     }
-
-    const files = [`${path}/id`];
-    const uploadResponse = await artifactClient.uploadArtifact(telepresenceIdArtifact, files, path, artifactOptionsUpload);
-    uploadResponse.failedItems.forEach(itemFailed => {
-        core.warning(`It was not possible to save the file: ${itemFailed}`);
-    });
 }
 
 
