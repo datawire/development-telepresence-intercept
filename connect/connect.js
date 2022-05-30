c8b22c53c17eb84f5de6037bad7ec16ec5bc20b2
Arturo Gonzalez Villa <arturogonzalez@datawire.io>
2022-05-30 12:42:46 -0500

    Add configuring action

--- connect/connect.js
+++ connect/connect.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 const configure = require('../src/configure');
 
 const telepresenceConnect = async function () {
-    !configure.validateId() && configure.getConfiguration();
+    await configure.getConfiguration();
     try {
         await exec.exec('telepresence', ['connect']);
     } catch (error) {
