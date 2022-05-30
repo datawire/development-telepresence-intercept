53de4066b7c438269bb4f1a94dee3083d03b7f2e
Arturo Gonzalez Villa <arturogonzalez@datawire.io>
2022-05-30 12:49:23 -0500

    Add configuring action

--- intercept/intercept.js
+++ intercept/intercept.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 const configure = require('../src/configure');
 
 const telepresenceIntercept = async function () {
-    !configure.validateId() && await configure.getConfiguration();
+    if (!configure.validateId()) await configure.getConfiguration();
     try {
         const service_name = core.getInput('service_name');
         const service_port = core.getInput('service_port');
