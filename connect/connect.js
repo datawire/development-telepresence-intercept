f23ff4dfc32a0fa72a9fb81e0b296ed391c9a04f
Arturo Gonzalez Villa <arturogonzalez@datawire.io>
2022-05-30 10:25:43 -0500

    Add configuring action

--- connect/connect.js
+++ connect/connect.js
@@ -1,22 +1,19 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const io = require('@actions/io');
-const toolCache = require('@actions/tool-cache');
+const cache = require('@actions/cache');
 const getTelepresenceConfigPath = require('./telepresenceConfigPath');
 
 const telepresenceConnect = async function () {
     const path = getTelepresenceConfigPath.getTelepresenceConfigPath();
-    const restorePath = [path];
 
     try {
         await io.mkdirP(path);
-        const telepresenceID = toolCache.find('telepresenceID', '1.0');
-        if (!telepresenceID)
+        const cacheid = cache.restoreCache([paths], getTelepresenceConfigPath.TELEPRESENCE_CACHE_KEY)
+        if (!cacheid)
             throw new Error('Unable to find a telepresence install id stored');
     } catch (error) {
-        core.saveState(getTelepresenceConfigPath.TELEPRESENCE_ID_STATE,
-            getTelepresenceConfigPath.TELEPRESENCE_ID_SAVES);
-        core.warning(`Unable to find the telepresence id: ${error}`);
+        core.setFailed(error);
     }
     try {
         await exec.exec('telepresence', ['connect']);
